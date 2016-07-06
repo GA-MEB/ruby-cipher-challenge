1ad5596e3f32dd46ff22987e6d700852061c0c86
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-07-06 14:46:30 -0400

    Add example solution to challenge
    Also, un-hide tests for Level 2 features.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,10 +1,50 @@
 
+## Pass Level 1 Tests
 def encode(string, rot=13)
   string.downcase!
   # Your Code Goes Here
+  keys = 'abcdefghijklmnopqrstuvwxyz'.chars
+  values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(rot)
+  pairs = keys.zip(values)
+  hsh = Hash[pairs]
+
+  string.chars.map{ |c| hsh[c] }.join
 end
 
 def decode(string, rot=13)
   string.downcase!
   # Your Code Goes Here
+  keys = 'abcdefghijklmnopqrstuvwxyz'.chars
+  values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(-1 * rot)
+  pairs = keys.zip(values)
+  hsh = Hash[pairs]
+
+  string.chars.map{ |c| hsh[c] }.join
 end
+
+## Pass Level 2 Tests
+# def encode(string, rot=13)
+#   string.downcase!
+#   # Your Code Goes Here
+#   keys = 'abcdefghijklmnopqrstuvwxyz'.chars
+#   values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(rot)
+#   pairs = keys.zip(values)
+#   hsh = Hash[pairs]
+#
+#   string.split.map do |word|
+#     word.chars.map{ |c| hsh[c] }.join
+#   end.join(' ')
+# end
+#
+# def decode(string, rot=13)
+#   string.downcase!
+#   # Your Code Goes Here
+#   keys = 'abcdefghijklmnopqrstuvwxyz'.chars
+#   values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(-1 * rot)
+#   pairs = keys.zip(values)
+#   hsh = Hash[pairs]
+#
+#   string.split.map do |word|
+#     word.chars.map{ |c| hsh[c] }.join
+#   end.join(' ')
+# end
--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe 'Challenge: ' do
         expect(encode('dog')).to eq('qbt')
       end
     end
-    xcontext 'level 2' do
-      xit 'properly encodes text with spaces' do
+    context 'level 2' do
+      it 'properly encodes text with spaces' do
         expect(encode('hello world')).to eq('uryyb jbeyq')
         expect(encode('the quick brown fox jumped over the lazy dog')).to eq('gur dhvpx oebja sbk whzcrq bire gur ynml qbt')
       end
-      xit 'can encode at an arbitrary rotation (ROT-N)' do
+      it 'can encode at an arbitrary rotation (ROT-N)' do
         expect(encode('abc def', 13)).to eq('nop qrs')
         expect(encode('abc def', 3)).to eq('def ghi')
         expect(encode('abc def', 1)).to eq('bcd efg')
@@ -31,12 +31,12 @@ RSpec.describe 'Challenge: ' do
         expect(encode('qbt')).to eq('dog')
       end
     end
-    xcontext 'level 2' do
-      xit 'properly decodes text with spaces' do
+    context 'level 2' do
+      it 'properly decodes text with spaces' do
         expect(decode('uryyb jbeyq')).to eq('hello world')
         expect(decode('gur dhvpx oebja sbk whzcrq bire gur ynml qbt')).to eq('the quick brown fox jumped over the lazy dog')
       end
-      xit 'can decode at an arbitrary rotation (ROT-N)' do
+      it 'can decode at an arbitrary rotation (ROT-N)' do
         expect(decode('nop qrs', 13)).to eq('abc def')
         expect(decode('def ghi', 3)).to eq('abc def')
         expect(decode('bcd efg', 1)).to eq('abc def')
