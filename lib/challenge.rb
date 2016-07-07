425af8e2f058fff2ea62db6991b1717272bd2a65
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-07-07 10:15:46 -0400

    Make variable names more semantic

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,48 +3,52 @@
 def encode(string, rot=13)
   string.downcase!
   # Your Code Goes Here
-  keys = 'abcdefghijklmnopqrstuvwxyz'.chars
-  values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(rot)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  keys = alphabet.chars
+  values = alphabet.chars.rotate(13)
   pairs = keys.zip(values)
-  hsh = Hash[pairs]
+  cipher = Hash[pairs]
 
-  string.chars.map{ |c| hsh[c] }.join
+  string.chars.map{ |c| cipher[c] }.join
 end
 
 def decode(string, rot=13)
   string.downcase!
   # Your Code Goes Here
-  keys = 'abcdefghijklmnopqrstuvwxyz'.chars
-  values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(-1 * rot)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  keys = alphabet.chars
+  values = alphabet.chars.rotate(-13) # or rotate(13)
   pairs = keys.zip(values)
-  hsh = Hash[pairs]
+  cipher = Hash[pairs]
 
-  string.chars.map{ |c| hsh[c] }.join
+  string.chars.map{ |c| cipher[c] }.join
 end
 
 ## Pass Level 2 Tests
 # def encode(string, rot=13)
 #   string.downcase!
 #   # Your Code Goes Here
-#   keys = 'abcdefghijklmnopqrstuvwxyz'.chars
-#   values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(rot)
+#   alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#   keys = alphabet.chars
+#   values = alphabet.chars.rotate(rot) # or rotate(13)
 #   pairs = keys.zip(values)
-#   hsh = Hash[pairs]
-#
+#   cipher = Hash[pairs]
+
 #   string.split.map do |word|
-#     word.chars.map{ |c| hsh[c] }.join
+#     word.chars.map{ |c| cipher[c] }.join
 #   end.join(' ')
 # end
-#
+
 # def decode(string, rot=13)
 #   string.downcase!
 #   # Your Code Goes Here
-#   keys = 'abcdefghijklmnopqrstuvwxyz'.chars
-#   values = 'abcdefghijklmnopqrstuvwxyz'.chars.rotate(-1 * rot)
+#   alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#   keys = alphabet.chars
+#   values = alphabet.chars.rotate(rot) # or rotate(13)
 #   pairs = keys.zip(values)
-#   hsh = Hash[pairs]
-#
+#   cipher = Hash[pairs]
+
 #   string.split.map do |word|
-#     word.chars.map{ |c| hsh[c] }.join
+#     word.chars.map{ |c| cipher[c] }.join
 #   end.join(' ')
 # end
